fe67e9b87b0706b45e577ec25145090a1b38e971
Bogdan L'gotin <dr34mway@gmail.com>
2025-01-26 21:22:17 +0600

    Add modules and auth index.html.

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+    "fmt"
+    "os"
+    "bufio"
+    "strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Auth() gin.HandlerFunc {
+    return func(ctx *gin.Context) {
+        ctx.Next()
+    }
+}
 
 func main() {
-    fmt.Println("Hello, world!")
+    router := gin.Default()
+    router.Use(Auth())
+
+    router.LoadHTMLFiles("auth/index.html")
+
+    router.GET("/auth", func(ctx *gin.Context) {
+        ctx.HTML(http.StatusOK, "index.html", gin.H{})
+    })
+
+    router.POST("/auth", func(ctx *gin.Context) {
+        fmt.Println(getAuthDataFromEnv())
+    })
+
+    router.Run(":8080")
 }
+
+func getAuthDataFromEnv() map[string]string {
+    envData := map[string]string{}
+
+    file, err := os.Open(".env")
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer file.Close()
+    
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        readData := strings.Split(scanner.Text(), "=")
+
+        envData[readData[0]] = readData[1]
+    }
+    
+    return envData
+}
+
